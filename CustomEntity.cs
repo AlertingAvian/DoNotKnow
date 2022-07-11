60f9c771bb5f6c38717dc8a7651ad85eac16bc7a
Patrick M <boyscoutpatrick813@gmail.com>
2022-07-10 21:51:50 -0500

    Undo changes for testing

--- CustomEntity.cs
+++ CustomEntity.cs
@@ -14,18 +14,14 @@ namespace Celeste.Mod.DoNotKnow
         public CustomEntity(EntityData data, Vector2 offset) : base(data.Position + offset) // NOT WORKING
         {
             Logger.Log(LogLevel.Info, "DoNotKnow", "Added instance of CustomEntity"); // this will probably never output. but it has
-            bank = new SpriteBank(GFX.Game, "Graphics/DoNotKnowSpriteBank.xml"); // added because I thought it might help somehow. it doesn't 
-            //this.sprite = GFX.SpriteBank.Create(); // and this prevents the level from loading, also gives useless error message about missing animimations
+            //bank = new SpriteBank(GFX.Game, "Graphics/DoNotKnowSpriteBank.xml"); // added because I thought it might help somehow. it doesn't 
+            this.sprite = GFX.SpriteBank.Create("Graphics/DoNotKnowSpriteBank.xml"); // and this prevents the level from loading, also gives useless error message about missing animimations
             //this.sprite.AddLoop("idle", "", 0.1f); // not needed if using a sprite bank
 
-            // however the atlases are made and custom sprites are done, is poorly documented and
-            // scattered across several different pages of docs with conflicting information so
-            // ive redone everything several times and at this point ive gone insane.
-            // i shouldn't have to ask for help on discord for something seemingly
-            // so basic.
+            // changed to allow aiden to test things
 
             base.Add(this.sprite);
-            //this.sprite.Play("idle"); // not sure if i need
+            this.sprite.Play("idle"); // not sure if i need
             base.Depth = -10000;
         }
 
